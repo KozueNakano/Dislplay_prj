e6262f6d21b1cbddc132011935d51717c14a04dc
kozue <maxon.onct@gmail.com>
2022-06-29 23:02:09 +0900

    delete no use files.

--- err_test.py
+++
@@ -1,5 +0,0 @@
-#!usr/bin/env python
-import sys
-
-print('err_exit!')
-sys.exit(1)
--- q.py
+++
@@ -1,8 +0,0 @@
-import sys
-import fcntl
-import termios
-
-cmd = "q"
-print(cmd)
-with open('/dev/tty1', mode='w') as f:
-    fcntl.ioctl(f, termios.TIOCSTI, cmd)
